0f43a8bb882fe6089779bb57ef252a35f4a66706
i9874 <i9874@NAMJI>
2020-10-15 19:55:27 +0900

    #20/10/15 19:55 관리자페이지 수정중 -njs-

--- semi_project_git/src/com/fs/admin/controller/PerfSsnEndServlet.java
+++ semi_project_git/src/com/fs/admin/controller/PerfSsnEndServlet.java
@@ -68,30 +68,6 @@ public class PerfSsnEndServlet extends HttpServlet {
 		
 		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
 		
-//		Calendar c = Calendar.getInstance();
-//		//일요일부터 (일:1,월:2,화:3,수:4,목:5,금:6,토:7)
-//		int fDay = 1; //일요일 뽑기
-//		int iDay = 2; //각 월 1일
-//		int week = 7; //일주일 기준
-//		c.set(1, 2020);
-//		c.set(2, 4);
-//		c.set(5, fDay);
-//		
-//		System.out.println("Calendar"+c.get(Calendar.DAY_OF_WEEK));
-//		
-//		if(c.get(Calendar.DAY_OF_WEEK)<iDay) {
-//			c.set(5, (iDay-c.get(Calendar.DAY_OF_WEEK))+fDay);
-//			System.out.println("C.day1"+c.get(5));
-//		}else if(c.get(Calendar.DAY_OF_WEEK)==iDay){
-//			c.set(5, fDay);
-//			System.out.println("C.day2"+c.get(5));
-//		}else {
-//			c.set(5, (week+iDay)-c.get(Calendar.DAY_OF_WEEK)+fDay);
-//			System.out.println("C.day3"+c.get(5));
-//		}
-		
-		
-	
 	}
 
 	/**
